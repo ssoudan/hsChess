c44db4da52ce9fb4ae13db14caa4df7326df35d6
Sebastien Soudan <sebastien@lyatiss.com>
2014-06-24 22:45:17 +0200

    [cleanup] moved to README.md

--- src/hsChess.hs
+++ src/hsChess.hs
@@ -14,29 +14,6 @@ import           TUI
 import           Utils
 
 
--- TODO:
---
---    [√] add manual strategy
---    [√] improve UX
---    [√] Mac packaging
---    [√] store the history in the State rather than the latest state (using DList)
---    [√] add castling moves
---    [√] web or GUI
---    [√] check detection and move selection
---    [√] mate detection
---    [√] show possible moves
---    [√] parallelize the evaluation of the options
---    [ ] async computation of moves and push notification on completion
---    [ ] stalemate detection
---    [ ] enter moves with DnD
---    [ ] test moves (shows best response moves of each player up to the horizon for a given option)
---    [ ] non-linear game history with bookmarks
---    [ ] pawn conversion
---    [ ] save/restore games
---    [ ] start from predefined states
---    [ ] gamification: Achievements to learn chess
-
-
 -- | Main function!
 main :: IO ()
 main = do
